87a4631e07f45e7a90b1d4d7b82bec4fbd378bd4
Sebastián Scialabba <sebastian.scialabba@appdirect.com>
2021-03-29 19:21:44 -0300

    Refactored cell visibility

---
+++ minesweeper-server/src/main/java/com/minesweeper/domain/cell/CellVisibleStatus.java
@@ -0,0 +1,14 @@
+package com.minesweeper.domain.cell;
+
+import java.util.Set;
+
+import com.google.common.collect.ImmutableSet;
+
+public enum CellVisibleStatus {
+	HIDDEN,
+	VISIBLE,
+	RED_FLAG,
+	QUESTION;
+
+	public static final Set<CellVisibleStatus> SUPPORTS_MARKING_RED_FLAG = ImmutableSet.of(HIDDEN, QUESTION);
+}
